43b33754f0449eee011e075813c18fd3930700de
Michael Fisher <michael.fisher.qaengineer@gmail.com>
2026-01-29 07:48:54 -0800

    fixed spacing, removed 'open country selector,' 'selectCountry' to be used instead

--- src/test/java/pageObjects/LoginScreen.java
+++ src/test/java/pageObjects/LoginScreen.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,53 +11,52 @@ import utilities.AppiumUtilities;
 public class LoginScreen extends BasePage {
 
 	WebDriver driver;
-	
-	//constructor
+
+	// constructor
 	public LoginScreen(WebDriver driver) {
 		super(driver);
-		
+
 	}
-	
-	//locators
-	
+
+	// locators
+
 	@FindBy(xpath = "//android.widget.TextView[@resource-id=\"com.androidsample.generalstore:id/toolbar_title\"]")
 	WebElement pageTitle;
-	
-	@FindBy(xpath="//android.widget.Spinner[@resource-id=\"com.androidsample.generalstore:id/spinnerCountry\"]")
+
+	@FindBy(xpath = "//android.widget.Spinner[@resource-id=\"com.androidsample.generalstore:id/spinnerCountry\"]")
 	WebElement countryDropdown;
 
-	@FindBy(xpath="//android.widget.TextView[@text=\\\"Your Name\\\"]")
+	@FindBy(xpath = "//android.widget.TextView[@text=\\\"Your Name\\\"]")
 	WebElement yourNameLabel;
-	
+
 	@AndroidFindBy(id = "com.androidsample.generalstore:id/nameField")
 	WebElement nameTextBox;
-	
+
 	@AndroidFindBy(id = "com.androidsample.generalstore:id/radioMale")
 	WebElement maleRadioButton;
-	
+
 	@AndroidFindBy(id = "com.androidsample.generalstore:id/radioFemale")
 	WebElement femaleRadioButton;
-	
+
 	@AndroidFindBy(id = "com.androidsample.generalstore:id/btnLetsShop")
 	WebElement letsShopButton;
-	
-	
-	//actions
-	
+
+	// actions
+
 	public String getPageTitle() {
 		return pageTitle.getText();
 	}
-	
+
 	public String getNameLabel() {
 		return yourNameLabel.getText();
 	}
-	
+
 	public void enterName(String name) {
 		nameTextBox.sendKeys(name);
 	}
-	
+
 	public void selectRadioButton(String gender) throws Exception {
-		switch(gender.toLowerCase()) {
+		switch (gender.toLowerCase()) {
 		case "male":
 			maleRadioButton.click();
 			break;
@@ -66,26 +64,18 @@ public class LoginScreen extends BasePage {
 			femaleRadioButton.click();
 			break;
 		default:
-			throw new Exception("Only male or female allowed for this selection.");	
+			throw new Exception("Only male or female allowed for this selection.");
 		}
 	}
-	
+
 	public void clickLetsShopButton() {
 		letsShopButton.click();
 	}
-	
-	public void openCountrySelector(String country) throws InterruptedException {
-		countryDropdown.click();
-//		AppiumUtilities.scrollAndroidElementIntoView(driver, country);
-//		driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"" + country + "\")")).click();
 
-	}
-	
 	public void selectCountry(WebDriver driver, String country) throws Exception {
 		countryDropdown.click();
 		AppiumUtilities.scrollAndroidElementIntoView(driver, country);
 		driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"" + country + "\")")).click();
-
 	}
 
 }
