6056fe3c6ffe2e8ee3f795f9d2b7a5e794ce9752
Michael Fisher <michael.fisher.qaengineer@gmail.com>
2026-01-29 08:44:57 -0800

    ensure we're using Appium elements, not selenium

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,14 +1,18 @@
 package pageObjects;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
 
 public class BasePage {
 	WebDriver driver;
 	
 	BasePage(WebDriver driver) {
 		this.driver = driver;
-		PageFactory.initElements(driver, this);
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
 	}
 
 }
