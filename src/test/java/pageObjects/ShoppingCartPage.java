5f298bc4e7bd306117955fcb664dece8d6e6bf99
Michael Fisher <michael.fisher.qaengineer@gmail.com>
2026-01-29 11:26:22 -0800

    udpates to shopping cart: fixed locator, code to check email checkbox

--- src/test/java/pageObjects/ShoppingCartPage.java
+++ src/test/java/pageObjects/ShoppingCartPage.java
@@ -5,24 +5,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ShoppingCartPage extends BasePage {
-WebDriver driver;
-	
+	WebDriver driver;
+
 	public ShoppingCartPage(WebDriver driver) {
 		super(driver);
 	}
-	
-	//locators
-	@FindBy(id = "android.widget.CheckBox")
+
+	// locators
+	@FindBy(className = "android.widget.CheckBox")
 	WebElement sendEmailsCheckbox;
-	
+
 	@FindBy(id = "com.androidsample.generalstore:id/btnProceed")
 	WebElement completePurchaseButton;
-	
+
 	@FindBy(id = "com.androidsample.generalstore:id/productName")
 	WebElement productNameInCart;
-	
+
 	public String getProductNameInCart() {
 		return productNameInCart.getText();
 	}
+
+	public void checkSendEmailsCheckbox() throws Exception {
+		if (sendEmailsCheckbox.getAttribute("checked").equalsIgnoreCase("checked")) {
+			return;
+		} else {
+			sendEmailsCheckbox.click();
+		}
+	}
 	
+	public void clickCompletePurchaseButton() {
+		completePurchaseButton.click();
+	}
+
 }
